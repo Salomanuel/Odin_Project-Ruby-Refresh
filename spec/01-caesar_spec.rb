c39e46d87db123841fe04dae81c87ed5618b17cb
Salomanuel <salomanuel.aux+code@gmail.com>
2017-11-17 17:50:27 +0100

    write many tests (RED)

--- spec/01-caesar_spec.rb
+++ spec/01-caesar_spec.rb
@@ -1,9 +1,29 @@
 require "01-caesar"
 
 describe CaesarCypher do
-
-	it "cyphers a simple word" do
-		expect(CaesarCypher("culo").to eql("oluc"))
+	before(:each) do
+		@alpha = ("a".."z").to_a
 	end
 
+
+	describe ".cypher" do
+
+		it "cyphers simple words" do
+			expect(CaesarCypher.cypher("culo")).to		eql("oluc")
+			expect(CaesarCypher.cypher("banana")).to 	eql("ananab")
+			expect(CaesarCypher.cypher("anna")).to	 	eql("anna")
+		end
+		
+		it "respects caps lock" do
+			skip
+			expect(CaesarCypher.cypher("CulO")).to 		eql("OluC")
+			expect(CaesarCypher.cypher("BaNana")).to	eql("anaNaB")
+			expect(CaesarCypher.cypher("Anna")).to		eql("annA")
+		end
+
+		it "deals with weird characters" do
+			skip
+			expect(CaesarCypher.cypher("!@#${%^&*()-=}))aNo")).to eql("aNo")
+		end
+	end
 end
