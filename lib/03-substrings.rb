0d8f13de7733cb9acf2e47b573a4092816d1dc9c
Salomanuel <salomanuel.aux+code@gmail.com>
2017-11-20 17:52:39 +0100

    substrings can deal one word

--- lib/03-substrings.rb
+++ lib/03-substrings.rb
@@ -1,10 +1,20 @@
 def substrings(word, dictionary)
-	return dictionary.include?(word)
+	occurrences = {}
+	dictionary.each do |word_2|
+		if word.include?(word_2)
+			if occurrences.key?(word_2)
+				occurrences[word_2] += 1
+			else
+				occurrences[word_2] = 1
+			end
+		end
+	end
+	return occurrences
 
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-# puts substrings("below", dictionary)
+puts substrings("below", dictionary)
 puts substrings("figa", dictionary)
