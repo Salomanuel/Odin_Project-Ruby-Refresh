0bf358d0bd0c22da9142aab23a9e7beffdb1ad48
Salomanuel <salomanuel.aux+code@gmail.com>
2017-11-20 17:41:49 +0100

    add 03-substrings in the readme

--- lib/03-substrings.rb
+++ lib/03-substrings.rb
@@ -1,3 +1,10 @@
-def substrings
+def substrings(word, dictionary)
+	return dictionary.include?(word)
 
 end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+# puts substrings("below", dictionary)
+puts substrings("figa", dictionary)
