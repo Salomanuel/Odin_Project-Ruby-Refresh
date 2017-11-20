447f5cde1be0fca6ca255e3f27299778df04885a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-11-20 18:03:13 +0100

    substring works with longer strings with many words

--- lib/03-substrings.rb
+++ lib/03-substrings.rb
@@ -1,20 +1,25 @@
-def substrings(word, dictionary)
+def substrings(string, dictionary)
 	occurrences = {}
-	dictionary.each do |word_2|
-		if word.include?(word_2)
-			if occurrences.key?(word_2)
-				occurrences[word_2] += 1
-			else
-				occurrences[word_2] = 1
+	string.split(" ").each do |string_word|
+		string_word.downcase!
+		dictionary.each do |dict_word|
+			if string_word.include?(dict_word)
+				if occurrences.key?(dict_word)
+					occurrences[dict_word] += 1
+				else
+					occurrences[dict_word] = 1
+				end
 			end
 		end
 	end
 	return occurrences
-
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-puts substrings("below", dictionary)
-puts substrings("figa", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+# puts substrings("below", dictionary)
+# puts substrings("figa", dictionary)
+
+ # { "down" => 1, "how" => 2, "howdy" => 1,"go" => 1, "going" => 1, "it" => 2, "i" => 3, "own" => 1,"part" => 1,"partner" => 1,"sit" => 1 }
