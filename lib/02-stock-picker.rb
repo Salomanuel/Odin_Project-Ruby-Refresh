0a4fbb7fb9199559dfcd9de68a238641d8a56b27
Salomanuel <salomanuel.aux+code@gmail.com>
2017-11-20 17:31:51 +0100

    refactor and add days capability

--- lib/02-stock-picker.rb
+++ lib/02-stock-picker.rb
@@ -1,24 +1,22 @@
 def stock_picker(prices)
-	best_deal = 0
-	buy_deal  = 0
-	sell_deal = 0
-	selling_prices = prices.dup
-	prices.each do |buy_price|
-		selling_prices.each do |sell_price|
-			deal      = sell_price - buy_price
+	best_deal 			= 0
+	sell_day_deal 	= 0
+	buy_day_deal  	= 0
+	selling_prices 	= prices.dup
+
+	prices.each_with_index do |buy_price, buy_day|
+		selling_prices.each_with_index do |sell_price, i|
+			deal     	  = sell_price - buy_price
 			if deal > best_deal
 				best_deal = deal 
-				buy_deal  = buy_price
-				sell_deal = sell_price
+				sell_day_deal = i + buy_day
+				buy_day_deal  = buy_day
 			end
-			puts "today we buy at #{buy_price} and sell at #{sell_price}"
-			puts "profit is #{sell_price - buy_price} and the best deal is #{best_deal}"
-			puts
 		end
 		selling_prices.shift
-		puts selling_prices.join(",")
 	end
-	return "the best gain is $#{best_deal}, selling for $#{sell_deal} and buying for $#{buy_deal}"
+	puts "the best gain is $#{best_deal}, selling for $#{prices[sell_day_deal]} and buying for $#{prices[buy_day_deal]}"
+	return [buy_day_deal, sell_day_deal]
 end
 
 
