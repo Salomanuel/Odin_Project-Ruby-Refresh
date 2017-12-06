bb935be6801cde84527a8926aee470f1e2a6a833
Salomanuel <salomanuel.aux+code@gmail.com>
2017-12-06 14:49:50 +0100

    implement #show_board, #is_it_free? and #move

--- lib/connect_four_manuel.rb
+++ lib/connect_four_manuel.rb
@@ -13,4 +13,23 @@ class Connect4
 		6.times { @board << row }
 	end
 
+	def show_board
+		@board.each do |row|
+			puts row.join(" ")
+		end
+		puts (0..6).to_a.join(" ")
+	end
+
+	def is_it_free?(line, column)
+		return true if @board[line][column] == "."
+		return false
+	end
+
+	def move(column, line=0)
+		@board[5][column] = "x"
+	end
+
+
 end
+
+# Connect4.new.show_board
--- spec/connect_four_manuel_spec.rb
+++ spec/connect_four_manuel_spec.rb
@@ -11,4 +11,33 @@ describe Connect4 do
 			expect(game.board).to eq fake_board
 		end
 	end
+
+	context "game" do
+		describe "#is_it_free?" do
+			it "returns true when it's free" do
+				expect(game.is_it_free?(5,2)).to eq true
+			end
+
+			it "returns false when it's not free" do
+				game.board[5] = %w(x x x x x x x)
+				expect(game.is_it_free?(5,1)).to eq false
+			end
+		end
+
+		describe "#move" do
+			it "occupies a full line" do
+				7.times { |i| game.move(i) }
+				expect(game.board[5]).to eq %w(x x x x x x x)
+			end
+		end
+	end
+
+	# context "Graphics" do
+	# 	it "shows an empty board nicely" do
+	# 		fake_board = 1.times do game.board.each { |row| puts row.join(" ") }
+	# 			puts (0..6).to_a.join(" ")
+	# 		end
+	# 		expect(game.show_board).to eq fake_board
+	# 	end
+	# end
 end
