9cca8936b43586915647149e967884b69eb53c5a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-12-22 14:04:03 +0100

    fibonacci works

--- lib/12-fibonacci.rb
+++ lib/12-fibonacci.rb
@@ -1,3 +1,18 @@
-def fibo(n)
-	return 0
+def fibo(n, result=0)
+	return 0 if n == 0
+	return 1 if n == 1
+	puts "with #{n} you have:#{result}"
+	return (fibo(n-1) + fibo(n-2))
 end
+
+# 10.times do |i|
+# 	puts "with #{i} is: #{fibo(i)}"
+# end
+
+result100 = 354224848179261915075
+result50 = 12586269025
+# puts fibo(50) == result50
+# puts fibo(50)
+puts fibo(30)
+# puts fibo(100) == result100
+# puts fibo(100)
--- spec/12-fibonacci_spec.rb
+++ spec/12-fibonacci_spec.rb
@@ -4,6 +4,12 @@ describe 'fibo' do
 	it 'return 0 with 0' do
 		expect(fibo(0)).to eq(0)
 	end
+	it 'returns the right numbers' do
+		fibonacci = [0,1,1,2,3,5,8,13,21]
+		fibonacci.each_with_index do |correct_result, i|
+			expect(fibo(i)).to eq(correct_result)
+		end
+	end
 end
 
 
